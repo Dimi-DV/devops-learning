8255ba9cd1b4ecc51910e330b8faa5bd2c4e344e
Dimitrije Davidovic Vedda <114122475+Dimi-DV@users.noreply.github.com>
2026-05-07 14:14:31 -0400

    Day 25: add Owner tag to VPC (#20)
    * Day 25: gate build-and-push to main-branch pushes only
    
    * Day 25: add matrix testing across Python 3.10/3.11/3.12
    
    * Day 25: extract Docker build/push into reusable workflow
    
    * Day 25: grant OIDC permissions on caller job, not workflow-wide
    
    * indentation fix
    
    * Day 25: add Terraform VPC CI workflow + IAM role/policy
    
    * Day 25: terraform fmt all VPC files
    
    * Day 25: add Owner tag to VPC for end-to-end CI test

--- week3/day15/aws-vpc-terraform/nat.tf
+++ week3/day15/aws-vpc-terraform/nat.tf
@@ -9,7 +9,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_1a.id      
+  subnet_id     = aws_subnet.public_1a.id
 
   tags = {
     Name        = "${var.project_name}-nat-gateway"
--- week3/day15/aws-vpc-terraform/routing.tf
+++ week3/day15/aws-vpc-terraform/routing.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name        = "${var.project_name}-public-rt"
@@ -8,19 +8,19 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public_internet" {
-    route_table_id         = aws_route_table.public.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id             = aws_internet_gateway.main.id
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_route_table_association" "public_1a" {
-    subnet_id              = aws_subnet.public_1a.id
-    route_table_id         = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_1b" {
-    subnet_id              = aws_subnet.public_1b.id
-    route_table_id         = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_1b.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table" "private" {
